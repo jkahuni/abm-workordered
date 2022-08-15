98031f54d07e8524ddbac12c0881c2eb254e7849
jkahuni <josephkahunimwangi@gmail.com>
2022-08-15 22:36:16 +0300

    .gitignore cleanup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: true,
+    firebase: {
+        projectId: 'abm-workordered',
+        appId: '1:78703146241:web:c4f54401a15015da0c1700',
+        databaseUrl: 'https://abm-workordered-default-rtdb.firebaseio.com',
+        storageBucket: 'abm-workordered.appspot.com',
+        locationId: 'us-central',
+        apiKey: 'AIzaSyCIw0V_Ms4mA4pZIfrUFL1h4Q0MuIjqobs',
+        authDomain: 'abm-workordered.firebaseapp.com',
+        messagingSenderId: '78703146241',
+        measurementId: 'G-C7WK73ZL6Y' 
+    },
+    africasTalkingCredentials: {
+        apiKey: 'fa8315549a493e578d6b1b4e611c3db3b7e2dfd1afcde5025f4ffd864b4b4a8e',
+        username: 'sandbox'
+    }
+}
